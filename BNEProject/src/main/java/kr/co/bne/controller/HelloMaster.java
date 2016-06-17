00a7551e84cfb3003889079f9bf5c8575528a398
jaewon <sffirefox@naver.com>
2016-06-17 21:44:46 +0900

    master branch change a code

--- BNEProject/src/main/java/kr/co/bne/controller/HelloMaster.java
+++ BNEProject/src/main/java/kr/co/bne/controller/HelloMaster.java
@@ -5,7 +5,7 @@ public class HelloMaster {
 	public static void main(String[] args) {
 		System.out.println("create Hello in master");
 		System.out.println("world's end dancehall");
-		
+		System.out.println("World's End dancehall");
 	}
 
 }
