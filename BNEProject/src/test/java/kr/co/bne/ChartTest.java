cadde11faaf87c5d1fbc7031cc85bf6b86cfba04
jaewon <sffirefox@naver.com>
2016-07-19 15:52:46 +0900

    !!

---
+++ BNEProject/src/test/java/kr/co/bne/ChartTest.java
@@ -0,0 +1,32 @@
+package kr.co.bne;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import kr.co.bne.dao.DailyReportDAO;
+import kr.co.bne.dto.DailyReportChartDTO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/test/resources/spring/applicationContext.xml"})
+@TransactionConfiguration(defaultRollback=true)
+public class ChartTest {
+	
+	@Autowired
+	DailyReportDAO dao;
+	
+	@Test
+	public void test() throws Exception {
+		List<DailyReportChartDTO> list = dao.selectDailyReportChartLineList("1");
+		
+		for(DailyReportChartDTO dto : list) {
+			System.out.println(dto.getEmployee_id() + ":" + dto.getReg_date());
+		}
+	}
+
+}
