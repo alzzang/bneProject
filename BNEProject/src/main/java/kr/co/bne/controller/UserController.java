a5c5ce132f69edbda6e5503147afd50363a3ba71
namericano <namericano@naver.com>
2016-08-25 09:27:42 +0900

    gow

--- BNEProject/src/main/java/kr/co/bne/controller/UserController.java
+++ BNEProject/src/main/java/kr/co/bne/controller/UserController.java
@@ -3,7 +3,6 @@ package kr.co.bne.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
@@ -13,10 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,18 +28,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-
-
 import kr.co.bne.dao.EmployeeDAO;
-
-
-
-
 import kr.co.bne.dto.DailyReportEmployeeDTO;
-
-
-
-
 import kr.co.bne.dto.EmployeeDTO;
 import kr.co.bne.service.CounsellingRecordService;
 import kr.co.bne.service.DailyReportService;
@@ -58,7 +51,11 @@ public class UserController {
 	@Autowired
 	CounsellingRecordService counsellingRecordService;
 
+	@Autowired
+	MessageChannel ftpChannel;
 
+	@Autowired
+	PollableChannel ftpChannel1;
 	
 	public void setFileName(String fileName, HttpServletRequest req){
 		HttpSession session = req.getSession();
@@ -76,54 +73,42 @@ public class UserController {
 	public void uploadFiles(HttpServletResponse response, MultipartHttpServletRequest req,
 			@RequestParam("id") String id) throws IOException {
 		String fileName = null;
+		
 		Map<String, MultipartFile> fileMap = req.getFileMap();
 		for (MultipartFile multipartFile : fileMap.values()) {
-			fileName=saveFileToLocalDisk(multipartFile, id);
+			fileName=saveFileToRemoteDisk(multipartFile, id);
 			userService.modifyFilePosition(id, fileName);
 		}
+		
 		setFileName(fileName,req);
-		
 	}
 
-	private String saveFileToLocalDisk(MultipartFile multipartFile, String id)
+
+	
+	private String saveFileToRemoteDisk(MultipartFile multipartFile, String id)
 			throws IOException, FileNotFoundException {
+
 		String temp[] = multipartFile.getContentType().split("/");
-		String outputFileName = getDestinationLocation() + id + "." + temp[1];
-		FileCopyUtils.copy(multipartFile.getBytes(), new FileOutputStream(outputFileName));
+		String outputFileName =  id + "." + temp[1];			
+		
+		File uploadFile = new File( outputFileName);
+		multipartFile.transferTo(uploadFile);
+		final Message<?> message = MessageBuilder.withPayload(uploadFile).build();
+		ftpChannel.send(message);
+		
+		
+		FileUtils.deleteQuietly(uploadFile.getParentFile());
+		
+		
+		
 		return id + "." + temp[1];
-	}
-
-	@RequestMapping(value = "/download/{fileName}", method = { RequestMethod.GET })
-	public void showFile(@PathVariable String fileName, HttpServletRequest req, HttpServletResponse res)
-			throws IOException {
-
-		byte[] buffer = new byte[1024];
-		int byteRead = -1;
-		FileInputStream inputStream;
-		OutputStream outStream;
-
-		File f = new File("c:/uploaded-files/" + fileName);
-
-		inputStream = new FileInputStream(f);
-		outStream = res.getOutputStream();
-
-		while ((byteRead = inputStream.read(buffer)) != -1) {
-			outStream.write(buffer, 0, byteRead);
-		}
-		inputStream.close();
-		outStream.close();
 		
 	}
+	
+	
+	
 
-	private String getDestinationLocation() {
-		String savedir = "c:/uploaded-files";
-		File saveDirFile = new File(savedir);
 
-		if (!saveDirFile.exists()) {
-			saveDirFile.mkdirs();
-		}
-		return "c:/uploaded-files/";
-	}
 
 	@RequestMapping(value = "/goLoginForm", method = { RequestMethod.GET })
 	public String goLoginForm() {
@@ -155,19 +140,14 @@ public class UserController {
 		EmployeeDTO employeeDTO = new EmployeeDTO();
 		employeeDTO = userService.validCheck(id, rawPassword);
 		String newpassword = req.getParameter("newpassword");
-		
-		
-		
+			
 		if (newpassword == null) {
 			if (employeeDTO != null) {
 				session.setAttribute("user", employeeDTO);
 				session.setAttribute("fileName", employeeDTO.getFile_position());
 
-
 				DailyReportEmployeeDTO employee= dailyReportService.searchPreSales(employeeDTO.getEmployee_id());
 
-				
-			
 				session.setAttribute("employee", employee);
 				return "redirect:/main";
 			}
@@ -188,17 +168,14 @@ public class UserController {
 	@RequestMapping(value = "/empSearch", method = { RequestMethod.POST })
 	public @ResponseBody List<EmployeeDTO> getEmpSearch(@RequestParam("empSearch") String empSearch ,HttpServletRequest req) {
 
-		
-		
-
-
+	
 			String temp="%"+empSearch+"%";
 			List<EmployeeDTO> list =userService.getEmpSearch(temp);
 			return list;
 	}
 
 	
-	/*    */
+	
 	@RequestMapping(value = "/searchUser/{empId}", method = { RequestMethod.GET })
 	public String showSearchUser(Model model,@PathVariable String empId, HttpServletRequest req, HttpServletResponse res)
 			throws IOException {
